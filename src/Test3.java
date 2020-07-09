ada5bc106e136aeb5fcdc1d75ea5f3d168121165
paulD-byte <65722799+paulD-byte@users.noreply.github.com>
2020-07-09 20:26:29 +0530

    update commit.....

---
+++ src/Test3.java
@@ -0,0 +1,16 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("Eat Someyhing......");
+		} else {
+			System.out.println("Do Work...........");
+		}
+	}
+
+}
